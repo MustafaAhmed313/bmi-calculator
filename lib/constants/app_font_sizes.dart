5854c1401ebb9026442ec41ace5271f6cb199952
Mustafa Ahmed <mostafa3132004@gmail.com>
2024-08-09 23:22:01 +0300

    Add App Constants. - colors. - font sizes. - font weights.

---
+++ lib/constants/app_font_sizes.dart
@@ -0,0 +1,8 @@
+class AppFontSizes {
+  static const num20 = 20; //semi bold, Regular, bold
+  static const num40 = 40; //bold
+  static const num10 = 10; //bold
+  static const num32 = 32; //semi bold
+  static const num64 = 64; //bold
+  static const num16 = 16; //medium
+}
